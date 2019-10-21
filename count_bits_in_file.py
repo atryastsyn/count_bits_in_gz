f0550c11b6effd271be4ec8e6895abae9d407654
goodhousekeeper <as.tryascyn@tensor.ru>
2019-10-21 14:44:13 +0300

    refactoring

--- count_bits_in_file.py
+++ count_bits_in_file.py
@@ -4,21 +4,20 @@ from functools import reduce
 
 FILE_NAME = 'test.gz'
 CHUNK_SIZE = 32
+BIT_TO_SEARCH = 1
 
 decompress_object = zlib.decompressobj(16 + zlib.MAX_WBITS)
-
 file_object = open(FILE_NAME,'rb')
-
 bit_count = 0
 
-def read_generator(f):
+def read_generator(f, d):
     while True:
         buffer=f.read(CHUNK_SIZE)
         if not buffer:
             break
-        yield list(decompress_object.decompress(buffer))
+        yield list(d.decompress(buffer))
 
-bit_count = reduce(lambda x, y: x + y, map(lambda x: x.count(1), read_generator(file_object)))
+bit_count = reduce(lambda x, y: x + y, map(lambda x: x.count(BIT_TO_SEARCH), read_generator(file_object, decompress_object)))
 
 print (bit_count)
 
