6e3c6bb604e41cc14e0cc920e3cdd42edb0ce899
goodhousekeeper <as.tryascyn@tensor.ru>
2019-10-21 17:22:23 +0300

    iterate through folder

--- count_bits_in_file.py
+++ count_bits_in_file.py
@@ -6,32 +6,29 @@ from functools import reduce
 
 FOLDER_PATH = os.path.join(os.getcwd(), 'storage')
 FILE_EXT = '.gz'
-FILE_NAME = 'test.gz'
 CHUNK_SIZE = 64
 BIT_TO_SEARCH = 1
 
-decompress_object = zlib.decompressobj(16 + zlib.MAX_WBITS)
 
-bit_count = 0
+# iterate through *.gz files in given folder
+def get_files_from_folder():
+    for entry in os.listdir(FOLDER_PATH):
+        path_to_entry = os.path.join(FOLDER_PATH, entry)
+        if os.path.isfile(path_to_entry) and entry.endswith(FILE_EXT):
+            yield(path_to_entry)
 
-def read_generator(f, d):
-    while True:
-        buffer=f.read(CHUNK_SIZE)
-        if not buffer:
-            break
-        yield list(d.decompress(buffer))
+# iterate through file chunk by chunk
+def get_file_content():
+    for path_to_file in get_files_from_folder():
+        file_object = open(path_to_file, 'rb')
+        decompress_object = zlib.decompressobj(16 + zlib.MAX_WBITS)
+        while True:
+            buffer=file_object.read(CHUNK_SIZE)
+            if not buffer:
+               file_object.close() 
+               break
+            yield list(decompress_object.decompress(buffer))
 
-for entry in os.listdir(FOLDER_PATH):
-    path_to_entry = os.path.join(FOLDER_PATH, entry)
-    if os.path.isfile(path_to_entry) and entry.endswith(FILE_EXT):
-        file_object = open(path_to_entry, 'rb')
-        bit_count += reduce(lambda acc, x: acc + x, 
-            map(lambda y: y.count(BIT_TO_SEARCH), 
-                 read_generator(file_object, decompress_object)), 0)
-               
-        file_object.close()           
+bit_count = reduce(lambda acc, x: acc + x, map(lambda y: y.count(BIT_TO_SEARCH), get_file_content()), 0)    
 
 print('Found {} ones'.format(bit_count))
-
-
-
