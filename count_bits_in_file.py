732b114d657572ba47874662996cae93d7e868cd
goodhousekeeper <as.tryascyn@tensor.ru>
2019-10-21 13:57:25 +0300

    mvp

--- count_bits_in_file.py
+++ count_bits_in_file.py
@@ -1,5 +1,6 @@
 import zlib
 import sys
+from functools import reduce
 
 FILE_NAME = 'test.gz'
 CHUNK_SIZE = 32
@@ -8,13 +9,18 @@ d = zlib.decompressobj(16 + zlib.MAX_WBITS)
 
 f=open(FILE_NAME,'rb')
 
+one_count = 0
+
+
 while True:
     buffer=f.read(CHUNK_SIZE)
     if not buffer:
         break
     chunk = list(d.decompress(buffer))
-    print(chunk)
+    ones = filter(lambda x:x==1, chunk )
+    one_count += reduce(lambda x,y: x + y, filter(lambda x:x==1, chunk ))
+    print(list(ones))
 
-
+print(one_count)
 f.close()
 
