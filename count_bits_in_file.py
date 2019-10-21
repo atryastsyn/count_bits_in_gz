5d42951311211006ca1450f3440f6737a81e5194
goodhousekeeper <as.tryascyn@tensor.ru>
2019-10-21 13:38:45 +0300

    read from gzip

--- count_bits_in_file.py
+++ count_bits_in_file.py
@@ -1,1 +1,20 @@
+import zlib
+import sys
+
 FILE_NAME = 'test.gz'
+CHUNK_SIZE = 32
+
+d = zlib.decompressobj(16 + zlib.MAX_WBITS)
+
+f=open(FILE_NAME,'rb')
+buffer=f.read(CHUNK_SIZE)
+
+while buffer:
+  chunk = list(d.decompress(buffer))
+  print(chunk)
+  
+  buffer=f.read(CHUNK_SIZE)
+
+
+f.close()
+
--- generate_binary_file.py
+++ generate_binary_file.py
@@ -3,12 +3,19 @@ import gzip
 
 FILE_LENGTH = 200
 ZEROES_PROBABLITY = 95
-FILE_NAME = 'test.gz'
+BIN_FILE_NAME = 'test.bin'
+GZ_FILE_NAME = 'test.gz'
+
 
 data = []
 
 for i in range(FILE_LENGTH):
     data.append(0 if random.randint(1,100) < ZEROES_PROBABLITY else 1)
 
-with gzip.open(FILE_NAME, 'wb') as f:
+with open(BIN_FILE_NAME, 'wb') as f:
     f.write(bytearray(data))
+    f.close()
+
+with gzip.open(GZ_FILE_NAME, 'wb') as f:
+    f.write(bytearray(data))
+    f.close()
