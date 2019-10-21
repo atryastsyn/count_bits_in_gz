358baa2196261bc9bf4d9cbd298e54f9daf5165d
goodhousekeeper <as.tryascyn@tensor.ru>
2019-10-21 15:08:50 +0300

    refactoring

--- generate_binary_file.py
+++ generate_binary_file.py
@@ -1,20 +1,15 @@
 import random
 import gzip
+import sys
 
 FILE_LENGTH = 200
 ZEROES_PROBABLITY = 95
-BIN_FILE_NAME = 'test.bin'
 GZ_FILE_NAME = 'test.gz'
-
 
 data = []
 
 for i in range(FILE_LENGTH):
     data.append(0 if random.randint(1,100) < ZEROES_PROBABLITY else 1)
-
-with open(BIN_FILE_NAME, 'wb') as f:
-    f.write(bytearray(data))
-    f.close()
 
 with gzip.open(GZ_FILE_NAME, 'wb') as f:
     f.write(bytearray(data))
