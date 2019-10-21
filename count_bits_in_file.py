4ef2cff8468f7b90826076bb2d7fb494888d2997
goodhousekeeper <as.tryascyn@tensor.ru>
2019-10-21 14:42:42 +0300

    use generator

--- count_bits_in_file.py
+++ count_bits_in_file.py
@@ -5,22 +5,22 @@ from functools import reduce
 FILE_NAME = 'test.gz'
 CHUNK_SIZE = 32
 
-d = zlib.decompressobj(16 + zlib.MAX_WBITS)
+decompress_object = zlib.decompressobj(16 + zlib.MAX_WBITS)
 
-f=open(FILE_NAME,'rb')
+file_object = open(FILE_NAME,'rb')
 
-one_count = 0
+bit_count = 0
 
+def read_generator(f):
+    while True:
+        buffer=f.read(CHUNK_SIZE)
+        if not buffer:
+            break
+        yield list(decompress_object.decompress(buffer))
 
-while True:
-    buffer=f.read(CHUNK_SIZE)
-    if not buffer:
-        break
-    chunk = list(d.decompress(buffer))
-    ones = filter(lambda x:x==1, chunk )
-    one_count += reduce(lambda x,y: x + y, filter(lambda x:x==1, chunk ))
-    print(list(ones))
+bit_count = reduce(lambda x, y: x + y, map(lambda x: x.count(1), read_generator(file_object)))
 
-print(one_count)
-f.close()
+print (bit_count)
+
+file_object.close()
 
