175c2519c1806f2adbabc014ca96becfc6ca529f
goodhousekeeper <as.tryascyn@tensor.ru>
2019-10-21 13:46:40 +0300

    refactoring

--- count_bits_in_file.py
+++ count_bits_in_file.py
@@ -7,13 +7,13 @@ CHUNK_SIZE = 32
 d = zlib.decompressobj(16 + zlib.MAX_WBITS)
 
 f=open(FILE_NAME,'rb')
-buffer=f.read(CHUNK_SIZE)
 
-while buffer:
-  chunk = list(d.decompress(buffer))
-  print(chunk)
-  
-  buffer=f.read(CHUNK_SIZE)
+while True:
+    buffer=f.read(CHUNK_SIZE)
+    if not buffer:
+        break
+    chunk = list(d.decompress(buffer))
+    print(chunk)
 
 
 f.close()
